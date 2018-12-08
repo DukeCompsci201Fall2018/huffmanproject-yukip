ea3c16ad4b0e11097db681cb277bbc376ae8018f
yupjpn <yuki.peters@duke.edu>
2018-12-08 02:52:29 -0500

    finish

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -63,19 +63,20 @@ public class HuffProcessor {
 	}
 
 	private void writeCompressedBits(String[] codings, BitInputStream in, BitOutputStream out) {
-		int bits = 0;
 		
 		while (true) {
+			int bits = in.readBits(BITS_PER_WORD);			
+
 			if (bits == -1) {
 				break;
-			}			
-			bits = in.readBits(BITS_PER_WORD); 
+			}	
+			
 			String code = codings[bits];
 			out.writeBits(code.length(), Integer.parseInt(code, 2));
 			
-			code = codings[PSEUDO_EOF];
-			out.writeBits(code.length(), Integer.parseInt(code, 2));
 		}
+		String code = codings[PSEUDO_EOF];
+		out.writeBits(code.length(), Integer.parseInt(code, 2));
 		
 	}
 
